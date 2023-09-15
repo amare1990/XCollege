4e5ad366708a07a825b2b7a359170aa17cb5bf70
amare1990 <amaremek@gmail.com>
2023-09-15 16:20:55 +0300

    Add the second navbar

--- XCollege/urls.py
+++ XCollege/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
