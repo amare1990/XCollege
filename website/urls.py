f5dd87ffed35f3332c2ca2dbc5ec601f6642e8d9
amare1990 <amaremek@gmail.com>
2023-09-18 15:16:22 +0300

    Add academics(BSc. in Computer Science) Page

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('admission/', views.admission, name='admission'),
+  path('academics/cs_bsc/', views.csBSc, name='cs-bsc'),
+  path('academics/se_bsc/', views.seBSc, name='se-bsc'),
+  path('academics/cs_msc/', views.csMSc, name='cs-msc'),
+  path('academics/se_msc/', views.seMSc, name='se-msc'),
+  path('academics/courses/', views.courses, name='courses-list'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'website/home.html', {})
 
+
+def admission(request):
+    return render(request, 'website/admission.html', {})
+
+def csBSc(request):
+    return render(request, 'website/cs_bsc.html')
+
+def seBSc(request):
+    return render(request, 'website/se_bsc.html')
+
+def csMSc(request):
+    return render(request, 'website/cs_bsc.html')
+
+def seMSc(request):
+    return render(request, 'website/cs_bsc.html')
+
+def courses(request):
+    return render(request, 'website/courses_list.html')
+
