092715af54c35d34d55ca61cea34fb462b2d1ad2
amare1990 <amaremek@gmail.com>
2023-09-21 10:36:09 +0300

    Add roles for the profile model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+roles = (
+  ('admin', 'admin'),
+  ('head', 'head'),
+  ('teacher', 'teacher'),
+  ('staff', 'staff'),
+  ('student', 'student')
+)
+
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  bio = models.TextField()
+  role = models.CharField(max_length=20, choices=roles, default='student')
+
+  def __str__(self):
+    return str(self.user)
