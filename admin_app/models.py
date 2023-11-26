c02247902621396d811f4d4ac1e567ed035ddf39
amare1990 <amaremek@gmail.com>
2023-11-26 13:29:09 +0300

    Create UserProfile model

--- admin_app/models.py
+++ admin_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from website.models import Profile
+from django.contrib.auth.models import User
 
 title_choices = (
   ('Mr.', 'Mr.'),
@@ -10,6 +11,45 @@ title_choices = (
   ('Associate Professor', 'Associate Professor')
 )
 
-class Teacher(Profile):
+year = (
+  ('1st', 'First'),
+  ('2nd', 'Second'),
+  ('3rd', 'Third'),
+  ('4th', 'Fourth'),
+  ('5th', 'Fifth')
+)
 
-  title = models.CharField(max_length=20, choices=title_choices)
+semester= (
+  ('1st', 'First'),
+  ('2nd', 'Second'),
+  ('summer', 'Summer')
+)
+
+roles = (
+  ('admin', 'admin'),
+  ('head', 'head'),
+  ('teacher', 'teacher'),
+  ('staff', 'staff'),
+  ('student', 'student')
+)
+
+
+class Department(models.Model):
+  name = models.CharField(max_length=50)
+  department_head = models.CharField(max_length=50)
+
+  def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default='default-text')
+    role = models.CharField(max_length=20, choices=roles, default='student')
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True, blank=True)
+    academic_year = models.CharField(max_length=20, choices=year, null=True, blank=True)
+    semester = models.CharField(max_length=20, choices=semester, null=True, blank=True)
+    title = models.CharField(max_length=20, choices=title_choices, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
