92387ba0909325a871ad49ce4cfbd345ed28a474
amare1990 <amaremek@gmail.com>
2023-11-26 13:30:31 +0300

    Create AddTeacherForm and AddStudentForm

--- admin_app/forms.py
+++ admin_app/forms.py
@@ -1,30 +1,15 @@
+# forms.py
 from django import forms
-from website.models import Profile
-from .models import Teacher
+from .models import UserProfile
 
-class AddProfileForm(forms.ModelForm):
-  class Meta:
-    model = Profile
-    fields = ['user', 'bio', 'role']
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'bio', 'role', 'department', 'academic_year', 'semester']
+        # You can customize the form fields and widgets as needed
 
-    widgets = {
-            'user': forms.Select(attrs={'class': 'form-control'}),
-            'bio': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'role': forms.Select(attrs={'class': 'form-control'}),
-        }
-
-
-# Add teacher
-class AddTeacherForm(forms.ModelForm):
-  class Meta:
-    model = Teacher
-    fields = ['user', 'title', 'bio', 'role']
-
-    widgets = {
-          'user': forms.Select(attrs={'class': 'form-control'}),
-          'title': forms.Select(attrs={'class': 'form-control'}),
-          'bio': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-          'role': forms.Select(attrs={'class': 'form-control'}),
-      }
-
-
+class TeacherForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'bio', 'role', 'department', 'title']
+        # You can customize the form fields and widgets as needed
