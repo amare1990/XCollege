43fbec7584a595650b859d35b1942226b89b6968
amare1990 <amaremek@gmail.com>
2024-01-04 16:31:15 +0300

    Modify styles of edit profile, register, login, and admin_dashboard pages

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from admin_app.models import UserProfile
-
-# User registration form
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
 from django.views import generic
@@ -18,36 +16,31 @@ class PasswordsChangeView(PasswordChangeView):
 def password_change_done(request):
   return render(request, 'accounts/registration/password_change_done.html', {})
 
-
 # User login
-# from django.http import Http404
-
 def user_login(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-
         if user is not None:
             login(request, user)
             try:
                 profile = UserProfile.objects.get(user=request.user)
             except UserProfile.DoesNotExist:
                 profile = None
-
             if profile is not None:
                 context = {'profile': profile}
-                messages.success(request, 'Bingo! %s' % (user.first_name))
-                if user.is_staff or profile.role == 'admin':
-                    return redirect('admin-dashboard')
-                elif profile.role == 'student':
-                    return render(request, 'accounts/student_profile.html', context)
-                elif profile.role == 'teacher' and profile.position is None:
-                    return render(request, 'accounts/teacher_profile.html', context)
-                elif profile.position == 'head':
-                    return render(request, 'accounts/head_profile.html', context)
-                elif profile.role is None or profile.position is None:
-                    return redirect('home')
+                messages.success(request, 'You have successfully logged in! %s' % (user.first_name))
+                # if user.is_staff or profile.role == 'admin':
+                return redirect('admin-dashboard')
+                # elif profile.role == 'student':
+                #     return render(request, 'accounts/student_profile.html', context)
+                # elif profile.role == 'teacher' and profile.position is None:
+                #     return render(request, 'accounts/teacher_profile.html', context)
+                # elif profile.position == 'head':
+                #     return render(request, 'accounts/head_profile.html', context)
+                # elif profile.role is None or profile.position is None:
+                #     return redirect('home')
             else:
                 # Handle the case where the UserProfile does not exist
                 messages.error(request, 'User profile does not exist')
