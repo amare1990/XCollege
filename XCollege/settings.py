d52d63e384d6cd99346c26645e33d9081d8e68a5
amare1990 <amaremek@gmail.com>
2024-02-16 11:30:17 +0300

    Update Settings.py file

--- XCollege/settings.py
+++ XCollege/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-from decouple import config,Csv #pip install python-decouple
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,7 +141,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 STATICFILES_DIRS = [
     BASE_DIR, 'static'
 ]
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
